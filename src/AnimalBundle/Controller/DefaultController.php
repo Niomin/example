36a725b81363261c755953bd673d8d5ca3ea92b6
Aleksandr Kosov <kosovalexander@yandex.ru>
2016-04-10 03:55:33 +0300

    add files

---
+++ src/AnimalBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AnimalBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @ Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AnimalBundle:Default:index.html.twig');
+    }
+}
